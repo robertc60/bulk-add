c3910915e93071910e64272e46747232c82d1491
Robert Clarkson <robertc60@live.com>
2021-08-20 14:42:49 +0100

    Added project

---
+++ hybrid calendar security group.ps1
@@ -0,0 +1,14 @@
+﻿. C:\scripts\Functions-PSStoredCredentials.ps1
+# pulls secure credential from the key path
+$Credential = Get-StoredCredential -UserName tenant admin
+# connect to msol
+Connect-MsolService -Credential $Credential
+# gets all users and filters out users with permission to create calendar items
+$users = Get-Mailbox -ResultSize unlimited -Filter {displayname -ne "Admin Users"} | Select UserPrincipalName
+#loops through users
+foreach ($user in $users){
+    # gets the user
+    $member = Get-MsolUser -UserPrincipalName $user.UserPrincipalName
+    # adds member to the mail enabled security group
+    Add-DistributionGroupMember -Identity "test" -Member $member.DisplayName
+}
